c6a8cfcf85e912dd40711ad8aae1838987214bbe
CarlosJair117 <carlos.fdez.guar19@gmail.com>
2021-03-14 23:05:38 -0600

    tercer commit

--- lib/src/pages/create_account.dart
+++ lib/src/pages/create_account.dart
@@ -19,7 +19,7 @@ class _AccountState extends State<Account> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      body: Stack(
+      body: ListView(
         children: <Widget>[
           Container(
             padding: const EdgeInsets.only(top: 20, bottom: 20),
@@ -28,124 +28,116 @@ class _AccountState extends State<Account> {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Image.asset("assets/images/logo.png", width: 150),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "PET HERE",
-                          style: TextStyle(fontSize: 30, color: Colors.white),
-                        ),
-                        Text("Mantén seguras a tus mascotas")
-                      ],
-                    ),
-                  ],
-                ),
+                _presentacion(),
                 Image.asset(
                   "assets/images/gato.png",
                   width: 150,
                 )
               ],
             ),
+          ), 
+          Container(
+            padding: EdgeInsets.all(30.0),
+            child: _form(),
           ),
-          Transform.translate(
-            offset: Offset(0, 135),
-            child: Center(
-              child: Card(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                margin: const EdgeInsets.all(20),
-                child: Form(
-                  key: formKey,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Text("Crear Cuenta", style: TextStyle(fontSize: 18)),
-                        TextFormField(
-                          decoration: InputDecoration(labelText: "Usuario:"),
-                          onSaved: (value) => nameValue = value,
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return "Llene este campo";
-                            } else {
-                              return null;
-                            }
-                          },
-                        ),
-                        TextFormField(
-                          decoration:
-                              InputDecoration(labelText: "Correo electrónico:"),
-                          onSaved: (value) => email = value,
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return "Llene este campo";
-                            } else {
-                              return null;
-                            }
-                          },
-                        ),
-                        TextFormField(
-                          decoration: InputDecoration(labelText: "Contraseña:"),
-                          onSaved: (value) => passwordValue = value,
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return "Llene este campo";
-                            } else {
-                              return null;
-                            }
-                          },
-                        ),
-                        
-                        SizedBox(
-                          height: 30,
-                        ),
-                        RaisedButton(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                          color: Colors.orange,
-                          textColor: Colors.white,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text("Crear Cuenta"),
-                            ],
-                          ),
-                          onPressed: () {},
-                        ),
-                        OutlineButton(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            borderSide: BorderSide(color: Colors.orange),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  "Ya tengo una cuenta",
-                                  style: TextStyle(color: Colors.orange),
-                                ),
-                              ],
-                            ),
-                            onPressed: () => Navigator.pop(context)),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          )
         ],
       ),
     );
   }
 
-   
+  Widget _presentacion(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Image.asset("assets/images/logo.png", width: 150),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "PET HERE",
+              style: TextStyle(fontSize: 30, color: Colors.white),
+            ),
+            Text("Mantén seguras a tus mascotas")
+          ],
+        ),
+      ],
+    );
+  }   
+
+  Widget _form(){
+    return Form(
+      key: formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("Crear Cuenta", style: TextStyle(fontSize: 18)),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Usuario:"),
+              onSaved: (value) => nameValue = value,
+              validator: (value) {
+                if (value.isEmpty) {
+                  return "Llene este campo";
+                } else {
+                  return null;
+                }
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Correo electrónico:"),
+              onSaved: (value) => email = value,
+              validator: (value) {
+                if (value.isEmpty) {
+                  return "Llene este campo";
+                } else {
+                  return null;
+                }
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Contraseña:"),
+              onSaved: (value) => passwordValue = value,
+              validator: (value) {
+                if (value.isEmpty) {
+                  return "Llene este campo";
+                } else {
+                  return null;
+                }
+              },
+            ),
+            SizedBox( height: 30,),
+            _crearCuenta(),
+            _regresar(),
+          ],
+        ),
+    );
+  }
+
+  Widget _crearCuenta(){
+    return RaisedButton(
+      shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(10)),
+      color: Colors.orange,
+      textColor: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text("Crear Cuenta"), ],
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget _regresar() {
+    return OutlineButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      borderSide: BorderSide(color: Colors.orange),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text("Ya tengo una cuenta",style: TextStyle(color: Colors.orange), ),],
+      ),
+      onPressed: () => Navigator.pop(context)
+    );
+  }
 
 }
 
